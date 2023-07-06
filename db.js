8b8baaeefda08f1dd8e5b20f443e3e7aa7cb6bc2
trishantjain <trishantjain8888@gmail.com>
2023-07-06 23:42:11 +0530

    Changed Database URL to Mongo Atlas

--- db.js
+++ db.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 const DB_URL = process.env.DB_URL;
-const mongoURI = `${DB_URL}/studLearn`
 
 const connectToMongo = async () => {
     try {
-        await mongoose.connect(mongoURI)
-        console.log("Mongoose Connected...");
+        mongoose.connect(DB_URL).then(() => {
+            console.log("DB Connected")
+        })
     } catch (err) {
-        console.error(err.message)
-        process.exit(1)
+        console.log("Not connected");
     }
 }
 
